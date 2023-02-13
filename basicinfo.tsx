51752958f38581c69712931e340f538265479bae
Quinten Bettin <122491208+qbettin@users.noreply.github.com>
2023-02-13 10:48:06 -0500

    did the stuff yuh (#1)
    Co-authored-by: Quinten <qbettin@udel.edu>

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,11 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+
+const yourName : String = "Quinten Bettin";
+let yourPronouns : String = "He/Him";
+const yourMajor : String = "CS BS";
+let classification : String = "sophmore";
+let oneExcite : String = "Website dev";
+let anyConcerns : String = "Getting lost";
+let funFact : String = "I can backflip on ground";
